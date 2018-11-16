cc57f07dd5757756c132740a7ad55d800299085e
MrBubsters <islandouchchris@gmail.com>
2018-11-16 12:43:35 -0600

    testing test
    tester

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("RootLayout.fxml"));
-			Scene scene = new Scene(root, 600, 800);
+			Scene scene = new Scene(root, 900, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Pin5ive");
