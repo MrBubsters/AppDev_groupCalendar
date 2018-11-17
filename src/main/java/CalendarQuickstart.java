16199558b89544cb705e22d20f4c9f562770e1bb
MrBubsters <islandouchchris@gmail.com>
2018-11-17 16:33:23 -0600

    Added Events
    Implemented Event constructor class
    created addEvent method to calendar

--- src/main/java/CalendarQuickstart.java
+++ src/main/java/CalendarQuickstart.java
@@ -12,17 +12,21 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.EventReminder;
 import com.google.api.services.calendar.model.Events;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class CalendarQuickstart {
-    private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
+    private static final String APPLICATION_NAME = "CalAPP";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
@@ -30,7 +34,7 @@ public class CalendarQuickstart {
      * Global instance of the scopes required by this quickstart.
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
-    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
+    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR);
     private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
     /**
@@ -39,6 +43,7 @@ public class CalendarQuickstart {
      * @return An authorized Credential object.
      * @throws IOException If the credentials.json file cannot be found.
      */
+    
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
         InputStream in = CalendarQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
@@ -52,6 +57,47 @@ public class CalendarQuickstart {
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+    
+    public static void addEvent(Calendar service) throws IOException {
+    	Event event = new Event()
+    		    .setSummary("test")
+    		    .setLocation("800 Howard St., San Francisco, CA 94103")
+    		    .setDescription("A chance to hear more about Google's developer products.");
+
+    		DateTime startDateTime = new DateTime("2018-11-28T09:00:00-07:00");
+    		EventDateTime start = new EventDateTime()
+    		    .setDateTime(startDateTime)
+    		    .setTimeZone("America/Chicago");
+    		event.setStart(start);
+
+    		DateTime endDateTime = new DateTime("2018-11-28T17:00:00-07:00");
+    		EventDateTime end = new EventDateTime()
+    		    .setDateTime(endDateTime)
+    		    .setTimeZone("America/Chicago");
+    		event.setEnd(end);
+
+    		String[] recurrence = new String[] {"RRULE:FREQ=DAILY;COUNT=2"};
+    		event.setRecurrence(Arrays.asList(recurrence));
+
+//    		EventAttendee[] attendees = new EventAttendee[] {
+//    		    new EventAttendee().setEmail("lpage@example.com"),
+//    		    new EventAttendee().setEmail("sbrin@example.com"),
+//    		};
+//    		event.setAttendees(Arrays.asList(attendees));
+
+    		EventReminder[] reminderOverrides = new EventReminder[] {
+    		    new EventReminder().setMethod("email").setMinutes(24 * 60),
+    		    new EventReminder().setMethod("popup").setMinutes(10),
+    		};
+    		Event.Reminders reminders = new Event.Reminders()
+    		    .setUseDefault(false)
+    		    .setOverrides(Arrays.asList(reminderOverrides));
+    		event.setReminders(reminders);
+
+    		String calendarId = "primary";
+    		event = service.events().insert(calendarId, event).execute();
+    		System.out.printf("Event created: %s\n", event.getHtmlLink());
     }
 
     public static void main(String... args) throws IOException, GeneralSecurityException {
@@ -82,5 +128,6 @@ public class CalendarQuickstart {
                 System.out.printf("%s (%s)\n", event.getSummary(), start);
             }
         }
+        addEvent(service);
     }
 }
