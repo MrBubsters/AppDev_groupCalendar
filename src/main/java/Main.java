3a48bd089c6720578cc5d5f20c86f1c04e00c5a5
MrBubsters <islandouchchris@gmail.com>
2018-11-28 20:26:54 -0600

    Fixed ClassNotFound Error
    Unknown issues in the fxml files was pointing to the wrong class. Issue seems to be fixed.
    
    showView() added to main to test new views. It adds the fxml to the center of the root layout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,54 @@
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+    private Stage primaryStage;
+    
+    private BorderPane rootLayout;
+
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("RootLayout.fxml"));
-			Scene scene = new Scene(root, 900, 600);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Pin5ive");
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+ 
+        this.primaryStage.setTitle("Pin5ive");
+ 
+        initRootLayout();
+ 
+        showView();
+    }
+ 
+    //Initializes the root layout.
+    public void initRootLayout() {
+        try {
+            rootLayout = FXMLLoader.load(getClass().getResource("RootLayout.fxml"));
+ 
+            Scene scene = new Scene(rootLayout, 700, 375);
+            primaryStage.setScene(scene);
+
+            primaryStage.show();
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+    public void showView() {
+        try {
+        	AnchorPane View = FXMLLoader.load(getClass().getResource("UserLogin.fxml"));
+        	
+            rootLayout.setCenter(View);
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 	public static void main(String[] args) {
 		launch(args);
