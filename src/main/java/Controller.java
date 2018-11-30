f182eee4cb6ac41962ac8d1cbc2cc0e8641579e0
MrBubsters <islandouchchris@gmail.com>
2018-11-30 14:45:15 -0600

    Update Controller.java

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,7 +1,43 @@
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-public class Controller {
+public class Controller extends Main {
 
+	@FXML Button ChangeMe;
+//	@FXML Button SignMeUp;
+
+	@FXML private void handleSubmitButtonAction(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new 
+		        FXMLLoader(getClass().getResource("RootLayout.fxml"));
+		    Parent root1 = (Parent) fxmlLoader.load();
+		    Stage stage = new Stage();
+		    //set what you want on your stage
+		    stage.initModality(Modality.APPLICATION_MODAL);
+		    stage.setTitle("Pin5ive Application");
+		    stage.setScene(new Scene(root1));
+		    stage.setResizable(false);
+		    stage.show();
+	}
 	
-}
+	@FXML private void SignUpButtonAction(ActionEvent event) throws IOException{
+		FXMLLoader fxmlLoader = new 
+		        FXMLLoader(getClass().getResource("SignUpSheet.fxml"));
+		    Parent root2 = (Parent) fxmlLoader.load();
+		    Stage stage = new Stage();
+		    //set what you want on your stage
+		    stage.initModality(Modality.APPLICATION_MODAL);
+		    stage.setTitle("Pin5ive SignUp Sheet");
+		    stage.setScene(new Scene(root2));
+		    stage.setResizable(false);
+		    stage.show();
+	
+	}
+    }
