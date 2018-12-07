a481612db72f36222651226f9ca6f22b3c60f368
bher010 <44444973+bher010@users.noreply.github.com>
2018-12-07 13:03:41 -0600

    Adding Root actions

--- src/main/java/RootLayoutController.java
+++ src/main/java/RootLayoutController.java
@@ -1,29 +1,24 @@
 
-
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 
+
 public class RootLayoutController {
-	 
-    //Exit the program
-    public void handleExit(ActionEvent actionEvent) {
+    
+	//Exit the program
+   public void handleExit(ActionEvent actionEvent) {
         // exit() causes the JavaFX application to terminate.
-    	Platform.exit();
-        
-        // exit(n) terminates the currently running Java Virtual Machine.
-        // Normal termination with n = 0
-        // A nonzero status code indicates abnormal termination.
-        //System.exit(0);
+    	System.exit(0);
+       
     }
  
     //Help Menu button behavior
-    public void handleHelp(ActionEvent actionEvent) {
+   public void handleHelp(ActionEvent actionEvent) {
         Alert alert = new Alert (Alert.AlertType.INFORMATION);
         alert.setTitle("Program Information");
-        alert.setHeaderText("This is a sample JAVAFX application");
+        alert.setHeaderText("For this Pin5ive Task Application");
         alert.setContentText("You can search, delete, update, " 
-        					 + "insert a new employee with this program.");
+        					 + "any task that you may have");
         alert.show();
     }
 }
